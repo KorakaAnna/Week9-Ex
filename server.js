3987dac1ca4188e9bd37ff4cbe30658096ab39db
KorakaAnna <akoraka23b@amcstudent.edu.gr>
2026-05-05 17:59:38 +0300

    Askisi 2

--- server.js
+++ server.js
@@ -10,3 +10,11 @@ app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
+const restaurants = [
+  { id: 1, name: "Taverna Giannis", location: "Αθήνα" },
+  { id: 2, name: "El Greco", location: "Θεσσαλονίκη" }
+];
+
+app.get('/restaurants', (req, res) => {
+  res.json(restaurants);
+});
